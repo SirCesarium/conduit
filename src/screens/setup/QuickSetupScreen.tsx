24603805d6329265838921083d7cda593cb68ff3
SirCesarium <cesarium3600@gmail.com>
2026-03-02 21:41:35 -0400

    feat: create quick setup screen content

--- src/screens/setup/QuickSetupScreen.tsx
+++ src/screens/setup/QuickSetupScreen.tsx
@@ -1,32 +1,51 @@
-import { EntityCard } from "../../components/EntityCard";
 import { CenteredLayout } from "../../layouts/CenteredLayout";
 import { Divider } from "../../components/Divider";
+import { Button } from "../../components/Button";
+import { useNav } from "../../routes";
+import { ArrowLeft } from "lucide-react";
+import { FeatureCard } from "../../components/FeatureCard";
 
 export const QuickSetupScreen = () => {
+  const { goBack } = useNav("quick_setup");
+
   return (
-    <CenteredLayout>
+    <CenteredLayout className="max-w-6xl">
       <header className="mb-20 flex flex-col items-center gap-4 text-center w-full">
-        <h1 className="text-2xl font-black uppercase tracking-[0.2em] leading-none">
+        <div className="fixed top-14 left-4">
+          <Button variant="ghost" onClick={goBack}>
+            <ArrowLeft />
+            BACK
+          </Button>
+        </div>
+
+        <h1 className="text-3xl font-black uppercase tracking-[0.2em]">
           QUICK SETUP
         </h1>
         <Divider className="mt-4" />
-        <p className="text-lg text-white/20 uppercase font-bold tracking-[0.4em] mt-4">
-          Placeholder
+        <p className="text-lg text-white/20 uppercase font-bold mt-4">
+          Select your gameplay environment
         </p>
       </header>
-
-      <main className="w-full space-y-4">
-        <EntityCard
-          title="PLACEHOLDER"
-          subtitle="PLACEHOLDER"
-          icon="Sparkles"
+      <main className="flex flex-row gap-6 w-full">
+        <FeatureCard
+          title="Vanilla"
+          subtitle="The classic experience"
+          icon="Pickaxe"
+          color="#bef264" // Lime
           onClick={() => {}}
         />
-        <EntityCard
-          title="PLACEHOLDER"
-          subtitle="PLACEHOLDER"
+        <FeatureCard
+          title="Plugins"
+          subtitle="Play with friends using extra tools and commands"
+          icon="Zap"
+          color="#fb923c" // Orange
+          onClick={() => {}}
+        />
+        <FeatureCard
+          title="Mods"
+          subtitle="Add new items, creatures, and adventures"
           icon="Box"
-          variant="outline"
+          color="#ef4444" // Red
           onClick={() => {}}
         />
       </main>
